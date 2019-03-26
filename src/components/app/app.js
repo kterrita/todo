097dd4dc6bcf962b4706afd3c30f5fa186c69143
Ilya Beleychev <ilya.beleychev@piano.io>
2019-03-26 10:09:35 +0300

    Search is added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ export default class App extends Component {
             this.createTodoItem('Drink coffee'),
             this.createTodoItem('Make awesome App'),
             this.createTodoItem('Have a lunch')
-        ]
+        ],
+        term: ''
     };
 
     createTodoItem(label) {
@@ -89,17 +90,52 @@ export default class App extends Component {
         });
     };
 
+    showAll = () => {
+        this.setState(({todoData}) => {
+            return {
+                todoData: todoData
+            };
+        });
+    };
+
+    searchItem(items, term) {
+
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.label.indexOf(term) > -1
+        });
+
+    }
+
+    onSearchChange = (term) => {
+        this.setState({term})
+    };
+
     render() {
+
+        const {todoData, term} = this.state;
+
+        const visibleItems = this.searchItem(todoData, term);
+        const doneCount = todoData.filter((el) => el.done).length;
+        const todoCount = todoData.length - doneCount;
+
         return (
             <div className="todo-app">
-                <AppHeader toDo={1} done={3}/>
+                <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className="top-panel d-flex">
-                    <SearchPanel/>
-                    <ItemStatusFilter/>
+                    <SearchPanel
+                        onSearchChange={this.onSearchChange}
+                    />
+                    <ItemStatusFilter
+                        showAll={this.showAll}
+                    />
                 </div>
 
                 <TodoList
-                    todos={this.state.todoData}
+                    todos={visibleItems}
                     onDeleted={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleDone={this.onToggleDone}
--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -7,10 +7,12 @@ export default class ItemStatusFilter extends Component {
         return (
             <div className="btn-group">
                 <button type="button"
-                        className="btn btn-info">All
+                        className="btn btn-info"
+                        onClick={this.props.showAll}>All
                 </button>
                 <button type="button"
-                        className="btn btn-outline-secondary">Active
+                        className="btn btn-outline-secondary"
+                        >Active
                 </button>
                 <button type="button"
                         className="btn btn-outline-secondary">Done
